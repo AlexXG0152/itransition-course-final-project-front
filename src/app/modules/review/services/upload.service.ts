a6f0726ca544216d617342bb3f7d943661f080f0
Alex <al.gr.0017@gmail.com>
2023-08-31 22:58:41 +0300

    feat: create review with photo upload

---
+++ src/app/modules/review/services/upload.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UploadService {
+  constructor(private http: HttpClient) {}
+
+  API = environment.API;
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  upload(formData: FormData) {
+    return this.http.post<any>(`${this.API}/firebase/upload`, formData, {
+      // reportProgress: true,
+      // observe: 'events',
+    });
+  }
+}
